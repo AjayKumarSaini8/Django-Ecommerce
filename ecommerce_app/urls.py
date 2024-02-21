d4aee10b0828ca927f6516a0503357958c807f8b
AjayKumarSaini8 <108985901+AjayKumarSaini8@users.noreply.github.com>
2024-02-21 15:13:46 +0530

    Update

--- ecommerce_app/urls.py
+++ ecommerce_app/urls.py
@@ -1,6 +1,17 @@
-from . import views
+# ecommerce_app/urls.py
 from django.urls import path
+from .views import (
+    get_all_products,
+    get_product_details,
+    add_to_cart,
+    get_cart_items,
+    remove_from_cart,
+)
 
 urlpatterns = [
-    path("", views.res, name=("res")),
+    path("products/", get_all_products, name="get_all_products"),
+    path("products/<int:id>/", get_product_details, name="get_product_details"),
+    path("cart/", get_cart_items, name="get_cart_items"),
+    path("cart/<int:id>/", remove_from_cart, name="remove_from_cart"),
+    path("cart/", add_to_cart, name="add_to_cart"),
 ]
--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -1,8 +1,44 @@
-from django.shortcuts import render
+# ecommerce_app/views.py
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Product, CartItem
+from .serializers import ProductSerializer, CartItemSerializer
 
-# Create your views here.
-from django.http import HttpResponse
+
+@api_view(["GET"])
+def get_all_products(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
 
-def res(request):
-    return HttpResponse("Hi there")
+@api_view(["GET"])
+def get_product_details(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+
+@api_view(["POST"])
+def add_to_cart(request):
+    # Assuming data in request is {'product_id': 1, 'quantity': 2}
+    serializer = CartItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+
+@api_view(["GET"])
+def get_cart_items(request):
+    cart_items = CartItem.objects.all()
+    serializer = CartItemSerializer(cart_items, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["DELETE"])
+def remove_from_cart(request, id):
+    cart_item = get_object_or_404(CartItem, pk=id)
+    cart_item.delete()
+    return Response(status=204)
